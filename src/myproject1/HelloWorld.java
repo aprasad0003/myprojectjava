94b074717b9f31787001edec593c3120348367da
aprasad0003 <anuj.prasad@irissoftware.com>
2019-11-18 14:25:36 +0530

    new

--- src/myproject1/HelloWorld.java
+++ src/myproject1/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello world");
+System.out.println("Hello world 123");
+System.out.println("Hello user");
 	}
 
 }
